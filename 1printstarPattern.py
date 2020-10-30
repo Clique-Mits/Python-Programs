748b9bba6a224be6e72c13f5d57eb6d32a3707a8
Jishnu P Sreekumar <psjishnu13@gmail.com>
2020-10-30 20:01:39 +0530

    added pattern print

--- 1printstarPattern.py
+++ 1printstarPattern.py
@@ -1,12 +1,12 @@
-n=int(input("enter number of lines: "))
+n = int(input("enter number of lines: "))
 
-#represents number of lines
-for i in range(0,n):
-    st=""
-    #represents no.of stars in each line.
-    for j in range(0,n):
-        if(j<i):
-            st+="  "
+# represents number of lines
+for i in range(0, n):
+    st = ""
+    # represents no.of stars in each line.
+    for j in range(0, n):
+        if(j < i):
+            st += "  "
         else:
-            st+="* "
+            st += "* "
     print(st)
