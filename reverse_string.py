8ddd24a83a0c91a2de9a291c0714ad727bbb29e9
Priyageorge123 <priyakgeorge4@gmail.com>
2020-10-31 13:35:43 +0530

    Reverse a string

---
+++ reverse_string.py
@@ -0,0 +1,11 @@
+def reverse(s): 
+  str = "" 
+  for i in s: 
+    str = i + str
+  return str
+  
+s = input()
+print ("The original string  is : ",end="") 
+print (s) 
+print ("The reversed string is : ",end="") 
+print (reverse(s)) 
