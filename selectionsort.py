0e5bcb8df8b2464372322a8d4568d19c318df9c4
psjishnu <psjishnu13@gmail.com>
2021-10-18 10:56:45 +0530

    add selectionsort

---
+++ selectionsort.py
@@ -0,0 +1,14 @@
+
+A = list(map(int, list(input().split())))
+
+for i in range(len(A)):
+    min_idx = i
+    for j in range(i+1, len(A)):
+        if A[min_idx] > A[j]:
+            min_idx = j
+
+    A[i], A[min_idx] = A[min_idx], A[i]
+
+print("Sorted array")
+for i in range(len(A)):
+    print("%d" % A[i]),
